a8f0a4493fff389429218b0d7f5deb4fc3b09319
Luiz Henrique Fernandes da Silva <luizhfernandes100@gmail.com>
2022-09-11 16:34:55 -0300

    Teste usando Git

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className="App">
-      <h1 className="text-7xl">Marcão do Flutter</h1>
+      <h1 className="text-7xl">Fazendo Testiamento do Git :)</h1>
     </div>
   )
 }
